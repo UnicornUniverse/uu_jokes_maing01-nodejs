019351ce9ee6e57b7c2f03bc73750fa74223c9a4
Martin Farkaš <martin.farkas@unicorn.com>
2022-08-17 13:56:20 +0200

    Switch to UuAppWorkspace.loadBasicData

--- uu_jokes_maing01-server/app/abl/jokes/load-abl.js
+++ uu_jokes_maing01-server/app/abl/jokes/load-abl.js
@@ -1,9 +1,10 @@
 "use strict";
 
 const { UuAppWorkspace } = require("uu_appg01_server").Workspace;
+const { AuthorizationService } = require("uu_appg01_server").Authorization;
 const { Validator } = require("uu_appg01_server").Validation;
 const { DaoFactory } = require("uu_appg01_server").ObjectStore;
-const { Config } = require("uu_appg01_server").Utils;
+const { UriBuilder } = require("uu_appg01_server").Uri;
 const Errors = require("../../api/errors/jokes-error");
 const InstanceChecker = require("../../component/instance-checker");
 const Constants = require("../constants");
@@ -49,29 +50,13 @@ class LoadAbl {
 
   async loadBasicData(uri, session, uuAppErrorMap = {}) {
     // HDS 1
+    let dtoOut = await UuAppWorkspace.loadBasicData(uri, session, uuAppErrorMap);
 
-    // ISSUE uuBT DEV Environment - Upgrade uuBT version to 2.15+
-    // https://uuapp.plus4u.net/uu-elementarymanagement-maing01/9c6fa609fb484a4c87861c927b3f5356/request?id=62de9dc9ed50f40028a471de
-    // const dtoOut = await UuAppWorkspace.loadBasicData(uri, session, uuAppErrorMap);
-
-    let awid = uri.getAwid();
-    let dtoOut = {};
-
-    const relatedObjectsMap = {
-      uuAppUuFlsBaseUri: Config.get("fls_base_uri"),
-      uuAppUuSlsBaseUri: Config.get("sls_base_uri"),
-      uuAppBusinessRequestsUri: Config.get("business_request_uri"),
-      uuAppBusinessModelUri: Config.get("business_model_uri"),
-      uuAppApplicationModelUri: Config.get("application_model_uri"),
-      uuAppUserGuideUri: Config.get("user_guide_uri"),
-      uuAppWebKitUri: Config.get("web_uri"),
-      uuAppProductPortalUri: Config.get("product_portal_uri"),
-    };
-
-    dtoOut.sysData = { relatedObjectsMap };
-
+    // HDS 2
+    const awid = uri.getAwid();
     const awidData = await UuAppWorkspace.get(awid);
 
+    // HDS 3
     if (
       awidData.sysState !== UuAppWorkspace.SYS_STATES.CREATED &&
       awidData.sysState !== UuAppWorkspace.SYS_STATES.ASSIGNED
@@ -80,10 +65,21 @@ class LoadAbl {
       dtoOut.data = { ...jokes, relatedObjectsMap: {} };
     }
 
-    dtoOut.territoryData = { data: {} };
-    dtoOut.uuAppErrorMap = uuAppErrorMap;
+    const cmdUri = UriBuilder.parse(uri).setUseCase("sys/uuAppWorkspace/load").clearParameters();
+    const authorizationResult = await AuthorizationService.authorize(session, cmdUri.toUri());
 
-    // HDS 2
+    const profileData = {
+      uuIdentityProfileList: authorizationResult.getIdentityProfiles(),
+      profileList: authorizationResult.getAuthorizedProfiles(),
+    };
+
+    // HDS 4
+    dtoOut.sysData = {
+      ...dtoOut.sysData,
+      awidData,
+      profileData,
+    };
+
     return dtoOut;
   }
 }
