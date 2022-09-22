1dcdc639856452028219948318f57f7ad66237de
Martin Farkaš <martin.farkas@unicorn.com>
2022-09-22 17:45:40 +0200

    Add CTRL + click support to route bar

--- uu_jokes_maing01-hi/src/core/route-bar.js
+++ uu_jokes_maing01-hi/src/core/route-bar.js
@@ -1,6 +1,7 @@
 //@@viewOn:imports
 import { createVisualComponent, useRoute, Lsi } from "uu5g05";
-import { useSubAppData, useSystemData } from "uu_plus4u5g02";
+import { useSubApp, useSubAppData, useSystemData } from "uu_plus4u5g02";
+import { Uri } from "uu_appg01_core";
 import Plus4U5App from "uu_plus4u5g02-app";
 
 import Config from "./config/config";
@@ -27,37 +28,47 @@ export const RouteBar = createVisualComponent({
     const [route, setRoute] = useRoute();
     const { data: system } = useSystemData();
     const { data: jokes } = useSubAppData();
+    const { baseUri } = useSubApp();
+
+    function handleActionClick(event, routeCode) {
+      if (event.ctrlKey || event.metaKey || event.button === 1) {
+        const uri = Uri.UriBuilder.parse(baseUri).setUseCase(routeCode).toUri();
+        window.open(uri);
+      } else {
+        setRoute(routeCode);
+      }
+    }
 
     const actionMap = {};
 
     if (system?.awidData?.sysState === Config.AppWorkspace.State.CREATED) {
       actionMap[Config.Routes.INIT_APP_WORKSPACE] = {
         children: <Lsi lsi={LsiData.initAppWorkspace} />,
-        onClick: () => setRoute(Config.Routes.INIT_APP_WORKSPACE),
+        onClick: (event) => handleActionClick(event, Config.Routes.INIT_APP_WORKSPACE),
       };
     } else {
       actionMap[Config.Routes.JOKES] = {
         children: <Lsi lsi={LsiData.jokes} />,
-        onClick: () => setRoute(Config.Routes.JOKES),
+        onClick: (event) => handleActionClick(event, Config.Routes.JOKES),
         icon: "mdi-emoticon-happy",
       };
 
       actionMap[Config.Routes.CATEGORIES] = {
         children: <Lsi lsi={LsiData.categories} />,
-        onClick: () => setRoute(Config.Routes.CATEGORIES),
+        onClick: (event) => handleActionClick(event, Config.Routes.CATEGORIES),
         icon: "mdi-shape",
       };
 
       actionMap[Config.Routes.CONTROL_PANEL] = {
         children: <Lsi lsi={LsiData.controlPanel} />,
-        onClick: () => setRoute(Config.Routes.CONTROL_PANEL),
+        onClick: (event) => handleActionClick(event, Config.Routes.CONTROL_PANEL),
         icon: "mdi-tune",
         collapsed: true,
       };
 
       actionMap[Config.Routes.ABOUT] = {
         children: <Lsi lsi={LsiData.about} />,
-        onClick: () => setRoute(Config.Routes.ABOUT),
+        onClick: (event) => handleActionClick(event, Config.Routes.ABOUT),
         icon: "mdi-information",
         collapsed: true,
       };
