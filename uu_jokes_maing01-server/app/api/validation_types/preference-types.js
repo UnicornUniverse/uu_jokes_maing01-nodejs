11c3302fe44700d554b226c9a364949e00937915
Martin Farkas <martin.farkas@unicorn.com>
2025-04-07 16:12:16 +0200

    Increase maximum size limit for data in preferenceCreateOrUpdateDtoInType

--- uu_jokes_maing01-server/app/api/validation_types/preference-types.js
+++ uu_jokes_maing01-server/app/api/validation_types/preference-types.js
@@ -15,5 +15,5 @@ const preferenceCreateOrUpdateDtoInType = shape({
   mtMainBaseUri: uri().isRequired(),
   code: string(/^\w{3,96}$/).isRequired(),
   scope: oneOf(["vendor", "app", "subApp", "uuAppWorkspace"]),
-  data: shape({}, true, 1000),
+  data: shape({}, true, 10000),
 });
